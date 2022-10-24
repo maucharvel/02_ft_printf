e24675e578a86cfc217a8fa77e7359519d9c5076
Mauricio Charvel <maucharvel@Mauricios-MacBook-Pro.local>
2022-10-24 14:35:22 +0200

    laptop test

--- ft_printf.c
+++ ft_printf.c
@@ -3,30 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcharvel <mcharvel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maucharvel <maucharvel@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 11:41:56 by mcharvel          #+#    #+#             */
-/*   Updated: 2022/10/24 12:13:48 by mcharvel         ###   ########.fr       */
+/*   Updated: 2022/10/24 14:34:16 by maucharvel       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
 
-int ft_printf(const char* format, ...)
+int	ft_printf(const char *format, ...)
 {
-    
-    va_list args;
-    int i;
-    
-    va_start(args, format);
-    i = vfprintf (stdout, format, args);
-    va_end (args);
+	va_list	args;
+	int		i;
 
-    return i;
+	va_start(args, format);
+	i = vfprintf (stdout, format, args);
+	va_end (args);
+
+	return (i);
 }
 
-int main(void)
+int	main(void)
 {
-    ft_printf("Hello\n");
+	ft_printf("Hello\n");
 }
