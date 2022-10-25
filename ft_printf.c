4f73ad895135c13ad12084ae282fb42bffcd9246
Mauricio Charvel Diaz <mcharvel@c3r5s6.42urduliz.com>
2022-10-25 19:12:40 +0200

    avance aitor

--- ft_printf.c
+++ ft_printf.c
@@ -3,28 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maucharvel <maucharvel@student.42.fr>      +#+  +:+       +#+        */
+/*   By: mcharvel <mcharvel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 11:41:56 by mcharvel          #+#    #+#             */
-/*   Updated: 2022/10/24 14:35:55 by maucharvel       ###   ########.fr       */
+/*   Updated: 2022/10/25 19:02:12 by mcharvel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
-
-int	ft_printf(const char *format, ...)
+#include <unistd.h>
+/* #include "./includes/ft_printf.h"
+#include "./libft/libft.h"
+ */
+int	ft_putchar(char c)
 {
-	va_list	args;
-	int		i;
+	write (1, &c, 1);
+	return (1);
+}
 
-	va_start(args, format);
-	i = vfprintf (stdout, format, args);
-	va_end (args);
+int	ft_putstr (char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
 	return (i);
+}
+
+static int	conv_sel(va_list *ap, char cs)
+{
+	int	count;
+
+	count = 0;
+	if (cs == '%')
+		count += ft_putchar('%');
+	else if (cs == 'c')
+		count += ft_putchar(va_arg(*ap, int));
+	else if (cs == 's')
+		count += ft_putstr(va_arg(*ap, char *));
+	return (count);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	va_list	args;
+
+	i = 0;
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%')
+			{
+				str++; // aqui estas en c
+				i += conv_sel(&args, *str);
+			}
+		else
+			i += ft_putchar(*str);
+		str++;
+	}
+	//return (i);
+va_end(args);
+return (i);
 }
 
 int	main(void)
 {
-	ft_printf("Hello\n");
+	int		i, a, b;
+	char	c;
+	char	*s;
+	
+	i = -123456;
+	c = 'K';
+	s = "Tequila";
+	a = printf("c: %c, s: %s\n", c, s);
+	b = ft_printf("c: %c, s: %s\n", c, s);
+
+	printf("printf: %i ft_printf: %i\n", a, b);
+
 }
+
