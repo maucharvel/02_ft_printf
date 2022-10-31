1b3231e108f4ebf5ddf47eb6637b6e36efd4ec86
Mauricio Charvel <maucharvel@Mauricios-MacBook-Pro.local>
2022-10-31 09:46:20 +0100

    tried %p, warning/error

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maucharvel <maucharvel@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 11:41:56 by mcharvel          #+#    #+#             */
-/*   Updated: 2022/10/28 09:20:43 by maucharvel       ###   ########.fr       */
+/*   Updated: 2022/10/31 09:44:49 by maucharvel       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,10 @@ static int	format_select(va_list *ap, char fs)
 		count += ft_putnbr(va_arg(*ap, int));
 	else if (fs == 'u')
 		count += ft_putnbr_un(va_arg(*ap, unsigned int));
- 	else if (fs == 'x' || fs == 'X') 
+	else if (fs == 'x' || fs == 'X')
+		count += ft_putnbrhexa(va_arg(*ap, size_t), fs);
+	else if (fs == 'p')
+		count += ft_putstr("0x");
 		count += ft_putnbrhexa(va_arg(*ap, size_t), fs);
 	return (count);
 }
@@ -135,8 +138,8 @@ int	main(void)
 	i = 563;
 	c = 'K';
 	s = "Mauricio";
-	a = printf("character: %c string: %s integer(int): %i integer(decimal): %d unsigned: %u Hexadecimal: %x\n", c, s, i, i, i, i);
-	b = ft_printf("character: %c string: %s integer(int): %i integer(decimal): %d unsigned: %u Hexadecimal: %x\n", c, s, i, i, i, i);
+	a = printf("character: %c string: %s integer(int): %i integer(decimal): %d unsigned: %u Hexadecimal: %x Pointer: %p\n", c, s, i, i, i, i, i);
+	b = ft_printf("character: %c string: %s integer(int): %i integer(decimal): %d unsigned: %u Hexadecimal: %x Pointer: %p\n", c, s, i, i, i, i, i);
 
 	printf("printf: %i ft_printf: %i\n", a, b);
 
